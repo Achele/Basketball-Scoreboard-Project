a60e65e270b3c924fc862540b9f01a677dd84ecc
Ojoachele Onuh <101713829+Achele@users.noreply.github.com>
2022-06-30 11:26:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,72 @@
+//Assigning values to variables.
 let homeScore = 0
 let guestScore = 0
 let guestFoul = 0
 let homeFoul = 0
 // let newGame = 0
 // let newGameEl = document.getElementById("new-game")
+
+//getting the id of the two score boards (home and guest) and the Fouls board
 let homeScoreEl = document.getElementById("home-score")
 let guestScoreEl = document.getElementById("guest-score")
 let homeFoulEl = document.getElementById("home-foul")
 let guestFoulEl = document.getElementById("guest-foul")
 // console.log(points)
 
+//  +1 home button
 function increaseHomeScoreOne() {
-   homeScore += 1
+    homeScore += 1
     homeScoreEl.textContent = homeScore
+    scoreBoardColor()
 }
-// add1()
+
+//  +2 home button
 function increaseHomeScoreTwo() {
-     homeScore += 2
+    homeScore += 2
     homeScoreEl.textContent = homeScore
+    scoreBoardColor()
 }
 
+//  +3 home button
 function increaseHomeScoreThree() {
-   homeScore += 3
+    homeScore += 3
     homeScoreEl.textContent = homeScore
+    scoreBoardColor()
 }
 
+//  +1 guest button
 function increaseGuestScoreOne() {
-     guestScore += 1
+    guestScore += 1
     guestScoreEl.textContent = guestScore
+    scoreBoardColor()
 }
 
+//  +2 guest button
 function increaseGuestScoreTwo() {
     guestScore += 2
     guestScoreEl.textContent = guestScore
+    scoreBoardColor()
 }
 
+//  +3 guest button
 function increaseGuestScoreThree() {
     guestScore += 3
     guestScoreEl.textContent = guestScore
+    scoreBoardColor()
 }
 
+//  +1 home foul
 function increaseHomeFoul() {
     homeFoul += 1
     homeFoulEl.textContent = homeFoul
 }
 
+//  +1 guest foul
 function increaseGuestFoul() {
     guestFoul += 1
     guestFoulEl.textContent = guestFoul
 }
+
 
 function gameStart() {
     // let newGame = 0
@@ -60,9 +78,20 @@ function gameStart() {
     guestFoul = 0
     guestScore = 0
     homeScore = 0
+    homeScoreEl.classList.remove("losing", "winning", "draw");
+    guestScoreEl.classList.remove("winning", "losing", "draw");
     // console.log(newGame)
     // document.getElementById("new-game-button").newGame = 0
 }
+
+/* function scoreColor() {
+if (homeScore < guestScore) {
+    homeScoreEl.backGroundColor = "darksalmon";
+} else {
+    homeScoreEl.backGroundColor = "green";
+}
+}
+scoreColor();*/
 
 // creating a function to highlight the score board color
 function scoreBoardColor()  {
